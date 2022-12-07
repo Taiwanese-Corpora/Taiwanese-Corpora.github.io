0dea5640b3c01ce0d708186f8c316d2d7c8e08e4
mhshih <simon.xian@gmail.com>
2022-12-07 15:20:45 +0800

    Developing segmented corpora

---
+++ segmented/segmented/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from klokahAPI import klokahAPI
+from django.shortcuts import render
+import csv
+from utilities import generic_concordancer
+
+nan_dir = 'corpora_root_dir/nan/'
+
+from nltk.util import bigrams
+import collections
+def query_results(request):
+
+    corpus_file = request.GET['corpus']
+    kw = request.GET['kw']
+
+
+    if corpus_file == '教育部閩南語辭典830650_11000900254_Attach1-19230筆詞義含詞類-斷詞11168.tsv':
+        target_column_num = 10
+        return HttpResponse('教育部閩南語辭典830650_11000900254_Attach1-19230筆詞義含詞類-斷詞11168.tsv')
+
+        sentences = [row[target_column_num] for row in csv.reader(open(nan_dir+corpus_file), delimiter="\t")]
+        rows = csv.reader(open(nan_dir+corpus_file), delimiter="\t")
+
+
+    elif kw:
+        output_rows = generic_concordancer(kw,rows,target_column_num)
+        return render(request, 'generic_concordance_list.htm', {"output_rows": output_rows})
+

55aef3e475098eb54e4d27ec440defacc280ad79
mhshih <d00142002@ntu.edu.tw>
2023-01-04 09:24:35 +0800

    Trivial comment on the simple home Response func:)

--- segmenter/segmenter/views.py
+++ segmenter/segmenter/views.py
@@ -1,13 +1,14 @@
-from django.http import HttpResponse
-
 from ArticutAPI_Taigi.ArticutAPI_Taigi import ArticutTG
-
 username = ""
 apikey = ""
 articutTG = ArticutTG(username, apikey)
 
+# Call with ../ArticutAPI_Taigi.ArticutAPI_Taigi(username, apikey).parse(text)
+# then output "result_segmentation" separated by space " ".
+
+from django.http import HttpResponse
 def home(request):
-    text = request.GET['text']
+    text = request.GET["text"]
     resultDICT = articutTG.parse(text)
-    output = resultDICT['result_segmentation']
-    return HttpResponse(output.replace("/", " "))
+    output = resultDICT["result_segmentation]
+    return HttpResponse(output.replace("/", " ")
